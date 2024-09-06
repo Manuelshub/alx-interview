e33b2d060187a7e5efa0697eb002e00896691528
Emmanuel <kcemmy03@gmail.com>
2024-09-06 03:38:38 +0100

    Modified function

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,27 +1,22 @@
 #!/usr/bin/node
 
-const request = require('request'); // Use the normal request module
+const util = require('util');
+const request = util.promisify(require('request')); // Use the normal request module
 const movieId = process.argv[2];
 
-function getCharacterNames (movieId) {
+async function getCharacterNames (movieId) {
   // Fetch the movie data
-  request.get(`https://swapi-api.alx-tools.com/api/films/${movieId}/`, { json: true }, (error, response, body) => {
-    if (error) {
-      return console.error('Error:', error.message);
-    }
+  let resp = await (await request(`https://swapi-api.alx-tools.com/api/films/${movieId}/`)).body;
 
-    const charactersUrls = body.characters;
+  resp = JSON.parse(resp);
+  const charactersUrls = resp.characters;
 
-    // Fetch each character's data
-    charactersUrls.forEach(url => {
-      request.get(url, { json: true }, (error, response, body) => {
-        if (error) {
-          return console.error('Error:', error.message);
-        }
-        console.log(body.name);
-      });
-    });
-  });
+  // Fetch each character's data
+  for (const url of charactersUrls) {
+    let character = await (await request(url)).body;
+    character = JSON.parse(character);
+    console.log(character.name);
+  }
 }
 
 getCharacterNames(movieId);
