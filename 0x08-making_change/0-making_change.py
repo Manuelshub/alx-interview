a6aabe2ee18c0ae6f20b9012b2546f0dbae491ac
Emmanuel <kcemmy03@gmail.com>
2024-09-19 14:11:17 +0100

    Added comments to my code

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -7,18 +7,25 @@ def makeChange(coins, total):
     """ Returns the fewest number of coins needed to
     meet total
     """
+    # Check to make sure input is valid
     if total <= 0:
         return 0
+    # Sorting coins in descending order
     coins = sorted(coins, reverse=True)
+    # Initializing a count
     count = 0
 
+    # Looping through coins
     for coin in coins:
         if total == 0:
             break
         if coin <= total:
+            # Getting the number of coins for each coin
             count += total // coin
+            # Updating total with the remaining amount
             total = total % coin
-
+    # If after the loop total is not 0, return -1
+    # Meaning that it is impossible to meet the total
     if total > 0:
         return -1
     return count
